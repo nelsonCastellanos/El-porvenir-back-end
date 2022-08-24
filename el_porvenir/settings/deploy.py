4bcda33fe4dd63be4e4fa7f95695481093902566
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2022-08-24 01:23:14 -0500

    Docker app

--- el_porvenir/settings/deploy.py
+++ el_porvenir/settings/deploy.py
@@ -1,9 +1,12 @@
 import os
 
 from . import *  # noqa: F403
+import dj_database_url
 
 # This is NOT a complete production settings file. For more, see:
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
