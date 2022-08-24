688dc36775bc5c6f9f7fa881baee9ff05ac001b0
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2022-08-24 01:03:47 -0500

    Docker app

---
+++ el_porvenir/settings/deploy.py
@@ -0,0 +1,9 @@
+import os
+
+from . import *  # noqa: F403
+
+# This is NOT a complete production settings file. For more, see:
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
